d4c45752ec439b00ddffa69800ddd4a730815ef1
EyalSel <ges@berkeley.edu>
2023-07-01 21:47:52 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sort",
+    version="0.1",
+    url='https://github.com/ICGog/sort',
+    install_requires=[
+        "scipy",
+        "filterpy==1.4.1",
+        "scikit-learn==0.19.2"
+    ],
+    packages=find_packages(exclude=["scripts*", "notebooks*"])
+)
